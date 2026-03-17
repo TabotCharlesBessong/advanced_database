208258e3c5629e69401735c2ba625021280dfd98
TabotCharlesBessong <ebezebeatrice@gmail.com>
2026-03-17 20:57:40 +0100

    Add SQL script to create employees table and select data

---
+++ advanced_portfolios/retail_store/mysql.session.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    hire_date DATE NOT NULL,
+    salary NUMERIC(10, 2) NOT NULL
+);
+
+SELECT * FROM employees;
